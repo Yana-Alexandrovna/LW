a4e344f6779040443986a108cc1be17aa53c7a6f
Yana-Alexandrovna <120241775+Yana-Alexandrovna@users.noreply.github.com>
2022-12-18 13:31:48 +0300

    С генератором

--- LW003/Program.cs
+++ LW003/Program.cs
@@ -1,3 +1,3 @@
-﻿int numberA = 3;
-int numberВ = 5;
-Console.WriteLine(numberA + numberВ );
+﻿int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberA + numberB );
